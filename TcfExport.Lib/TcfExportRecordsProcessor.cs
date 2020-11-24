a358d8294e9027da9b0a2865465694174c232f10
Derrick Granowski <granowski@coffeetariat.net>
2020-11-23 22:39:42 -0700

    forgot to check in the renamed file

---
+++ TcfExport.Lib/TcfExportRecordsProcessor.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+
+namespace TcfExport.Lib
+{
+  public class TcfExportRecordsProcessor : List<TcfExportRecord>
+  {
+    ILogger<TcfExportRecordsProcessor> _logger;
+
+    public TcfExportRecordsProcessor(ILogger<TcfExportRecordsProcessor> logger)
+    {
+      _logger = logger;
+    }
+    
+    public TcfExportDataList GetDataList()
+    {
+      TcfExportDataList list = new TcfExportDataList();
+
+      var records = this.Select(r =>
+      {
+        decimal
+          creditDecimal = 0,
+          debitDecimal = 0;
+
+        // todo -> inject logger, log warnings of failed parsing
+        bool successfulCreditParse = decimal.TryParse(r.Credit, out creditDecimal);
+        bool successfulDebitParse = decimal.TryParse(r.Debit, out debitDecimal);
+
+        if (!successfulCreditParse)
+          _logger.LogWarning("found value credit value '{0}' that could not be parsed to a decimal; defaulting to 0", r.Credit);
+        
+        if (!successfulDebitParse)
+          _logger.LogWarning("found value credit value '{0}' that could not be parsed to a decimal; defaulting to 0", r.Debit);
+        
+        return new TcfExportData
+        {
+          Credit = creditDecimal,
+          Debit = debitDecimal,
+          Date = r.Date
+        };
+      });
+
+      list.AddRange(records);
+      
+      return list;
+    }
+  }
+}
